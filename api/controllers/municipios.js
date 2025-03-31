0a31272d90e5ef84f0923c1f3d0e135a15c8fd85
Fatec Votorantim <159955708+fatec-votorantim@users.noreply.github.com>
2025-03-31 20:45:57 -0300

    validations

--- api/controllers/municipios.js
+++ api/controllers/municipios.js
@@ -1,16 +1,34 @@
 import { ObjectId } from "mongodb";
 
-export const getMunicipiosById = async(req, res)=>{
-    try{
-        const {id} = req.params
+export const getMunicipiosById = async (req, res) => {
+    try {
+        const { id } = req.params
         const db = req.app.locals.db
-        const municipio = await db.collection('municipios').findOne({_id: ObjectId.createFromHexString(id)})
-        if(!municipio){
-            return res.status(404).json({error: true, message: 'Munícipio não encontrado'})
+        const municipio = await db.collection('municipios').findOne({ _id: ObjectId.createFromHexString(id) })
+        if (!municipio) {
+            return res.status(404).json({ error: true, message: 'Munícipio não encontrado' })
         }
         res.status(200).json(municipio)
-    } catch (error){
-        res.status(500).json({error: true, message: 'Falha no servidor ao obter os municipios'})
+    } catch (error) {
+        res.status(500).json({ error: true, message: 'Falha no servidor ao obter os municipios' })
         console.error(error)
+    }
+}
+
+export const createMunicipio = async (req, res) => {
+    try {
+        const { codigo_ibge, nome, capital, codigo_uf, local } = req.body
+        const db = req.app.locals.db
+        //Verificar se já existe um municipio com o IBGE informado
+        const existeMunicipio = await db.collection('municipios').findOne({ codigo_ibge })
+        if (existeMunicipio) {
+            return res.status(409).json(({ error: true, message: 'Já existe um munícipio com o código IBGE informado!' })) //409-conflict
+        }
+        const novoMunicipio = { codigo_ibge, nome, capital, codigo_uf, local }
+        const result = await db.collection('municipios').insertOne(novoMunicipio)
+        res.status(201).json({ _id: result.insertedId, ...novoMunicipio })//201-created
+    } catch (error) {
+        console.error('Erro no municipio', error)
+        res.status(500).json({ error: true, message: 'Falha no servidor ao inserir o municipio' })
     }
 }
--- api/routes/municipios.js
+++ api/routes/municipios.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import {getMunicipiosById} from '../controllers/municipios.js'
-
+import {getMunicipiosById, createMunicipio} from '../controllers/municipios.js'
+import { validateMunicipio } from '../middleware/validations.js'
 const router = express.Router()
 
 //GET Municipio by id
 router.get('/:id', getMunicipiosById)
+//POST Cria um novo municipio
+router.post('/', validateMunicipio, createMunicipio)
 
 export default router
